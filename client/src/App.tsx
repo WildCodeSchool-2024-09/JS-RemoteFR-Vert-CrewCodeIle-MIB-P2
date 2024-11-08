ecb29c3f44c25711ef9aeb8c3cd6c4615c5ace8e
MATHIEU FOUCAULT <foucault.mathieu.pro@gmail.com>
2024-11-08 15:14:37 +0100

    fix : resolving biome errors #1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,13 +6,11 @@ import Header from "./components/Header";
 function App() {
   return (
     <>
-
       <main className="container">
         <ArticlesContainer />
       </main>
 
       <Header />
-
     </>
   );
 }
