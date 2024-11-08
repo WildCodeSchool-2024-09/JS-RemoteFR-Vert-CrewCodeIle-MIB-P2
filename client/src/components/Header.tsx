5fc99d6e54581191b7d573962aca7fd341f4e07a
Benjamin Deknuydt <deknuydtbenjamin@hotmail.fr>
2024-11-08 09:30:06 +0100

    feat: add logo, title, button

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header>
+      <img src="/src/assets/images/logoMIBnews.webp" alt="logo MIB News" />
+      <h1>M.I.B News</h1>
+      <button type="button">❤️Favoris</button>
+    </header>
+  );
+}
