94b761a6b2f83b3776171a66e0aad648ce8af87f
Benjamin Deknuydt <deknuydtbenjamin@hotmail.fr>
2024-11-08 11:35:02 +0100

    fix: biome

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,9 +1,17 @@
 export default function Header() {
-  return (
-    <header>
-      <img src="/src/assets/images/logoMIBnews.webp" alt="logo MIB News" />
-      <h1>M.I.B News</h1>
-      <button type="button">❤️Favoris</button>
-    </header>
-  );
+	return (
+		<header>
+			<img
+				src="/src/assets/images/logoMIBnews.webp"
+				alt="logo MIB News"
+				className="logo"
+			/>
+			<div className="titlebtn">
+				<h1>M.I.B News</h1>
+				<button type="button" className="favoris">
+					❤️Favoris
+				</button>
+			</div>
+		</header>
+	);
 }
