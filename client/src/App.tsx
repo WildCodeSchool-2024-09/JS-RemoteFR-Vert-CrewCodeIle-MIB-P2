b050712bd3847d79f9579c1281ceb3d42d50d4c7
Benjamin Deknuydt <deknuydtbenjamin@hotmail.fr>
2024-11-12 09:40:58 +0100

    style: footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,19 @@
 import "./App.css";
 
 import ArticlesContainer from "./components/ArticlesContainer";
-import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
-	return (
-		<>
-			<Header />
-			<main className="container">
-				<ArticlesContainer />
-			</main>
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <Header />
+      <main className="container">
+        <ArticlesContainer />
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
