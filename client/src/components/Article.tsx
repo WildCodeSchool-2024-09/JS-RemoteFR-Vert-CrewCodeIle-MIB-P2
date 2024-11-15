ab07357decffb8f44f5af437129fbf2b5cb9ee4b
MATHIEU FOUCAULT <foucault.mathieu.pro@gmail.com>
2024-11-15 11:45:41 +0100

    fix : resolving biome type error closes#8

--- client/src/components/Article.tsx
+++ client/src/components/Article.tsx
@@ -1,17 +1,16 @@
 import "./Article.css";
 
-// biome-ignore lint/style/useImportType: <explanation>
-import { ArticleType } from "../lib/definitions";
+import type { ArticleType } from "../lib/definitions";
 
 const Article = ({ id, webTitle, webUrl }: ArticleType) => {
-  return (
-    <section className="article-card" key={id}>
-      <h3>{webTitle}</h3>
-      <a href={webUrl} target="_blank" rel="noopener noreferrer">
-        Lire l'article
-      </a>
-    </section>
-  );
+	return (
+		<section className="article-card" key={id}>
+			<h3>{webTitle}</h3>
+			<a href={webUrl} target="_blank" rel="noopener noreferrer">
+				Lire l'article
+			</a>
+		</section>
+	);
 };
 
 export default Article;
