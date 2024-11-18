34e946b5adf3d978b7be71734d5ab98d634aeb44
Benjamin Deknuydt <deknuydtbenjamin@hotmail.fr>
2024-11-18 17:20:22 +0100

    style: button menu burger

--- client/src/components/MenuBurger.tsx
+++ client/src/components/MenuBurger.tsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
+import "./MenuBurger.css";
+
 export default function MenuBurger() {
+  const [open, setOpen] = useState(false);
+
+  const handleclick = () => {
+    setOpen(!open);
+  };
+
   return (
-    <div>
-      <button type="button">
-        <span> </span>
-      </button>
-    </div>
+    <nav>
+      <div className="burger openNav">
+        <button type="button" className="btnBurger" onClick={handleclick}>
+          <span className={`burgerNav ${open ? "open" : ""}`}> </span>
+        </button>
+      </div>
+    </nav>
   );
 }
