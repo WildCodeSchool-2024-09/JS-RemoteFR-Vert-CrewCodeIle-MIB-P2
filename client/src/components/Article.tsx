8b9f121c5529ee666f7de34ec9b940715743398c
MATHIEU FOUCAULT <foucault.mathieu.pro@gmail.com>
2024-11-20 12:06:29 +0100

    fix : API problems with pillarName and sortBar button closes#5

--- client/src/components/Article.tsx
+++ client/src/components/Article.tsx
@@ -4,48 +4,48 @@ import { useEffect, useState } from "react";
 import type { ArticleType } from "../lib/definitions";
 
 const collectFavorites = () => {
-	const favorites = localStorage.getItem("favorites");
-	return favorites ? JSON.parse(favorites) : ([] as string[]);
+  const favorites = localStorage.getItem("favorites");
+  return favorites ? JSON.parse(favorites) : ([] as string[]);
 };
 
 const Article = ({ id, webTitle, webUrl }: ArticleType) => {
-	const [isFavorite, setIsFavorite] = useState<boolean>(false);
+  const [isFavorite, setIsFavorite] = useState<boolean>(false);
 
-	const updateFavorites = (updatedFavorites: string[]) => {
-		localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
-		setIsFavorite(updatedFavorites.includes(id));
-	};
+  const updateFavorites = (updatedFavorites: string[]) => {
+    localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+    setIsFavorite(updatedFavorites.includes(id));
+  };
 
-	useEffect(() => {
-		const favorites = collectFavorites();
-		setIsFavorite(favorites.includes(id));
-	}, [id]);
+  useEffect(() => {
+    const favorites = collectFavorites();
+    setIsFavorite(favorites.includes(id));
+  }, [id]);
 
-	const toggleFavorite = (id: string) => {
-		const favorites = collectFavorites();
-		if (favorites.includes(id)) {
-			const newFavorites = favorites.filter((favId: string) => favId !== id);
-			updateFavorites(newFavorites);
-		} else {
-			updateFavorites([...favorites, id]);
-		}
-	};
+  const toggleFavorite = (id: string) => {
+    const favorites = collectFavorites();
+    if (favorites.includes(id)) {
+      const newFavorites = favorites.filter((favId: string) => favId !== id);
+      updateFavorites(newFavorites);
+    } else {
+      updateFavorites([...favorites, id]);
+    }
+  };
 
-	return (
-		<section className="article-card" key={id}>
-			<h3>{webTitle}</h3>
-			<a href={webUrl} target="_blank" rel="noopener noreferrer">
-				Lire l'article
-			</a>
-			<button
-				className="favorite-button"
-				type="button"
-				onClick={() => toggleFavorite(id)}
-			>
-				{isFavorite ? "❤️" : "🖤"}
-			</button>
-		</section>
-	);
+  return (
+    <section className="article-card" key={id}>
+      <h3>{webTitle}</h3>
+      <a href={webUrl} target="_blank" rel="noopener noreferrer">
+        Lire l'article
+      </a>
+      <button
+        className="favorite-button"
+        type="button"
+        onClick={() => toggleFavorite(id)}
+      >
+        {isFavorite ? "❤️" : "🖤"}
+      </button>
+    </section>
+  );
 };
 
 export default Article;
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -6,28 +6,28 @@ import type { ArticleType } from "../lib/definitions";
 import "../components/HomePage.css";
 
 const HomePage = () => {
-	const articles: ArticleType[] = useLoaderData() as ArticleType[];
+  const articles: ArticleType[] = useLoaderData() as ArticleType[];
 
-	const [currentCategory, setCurrentCategory] = useState<string>("");
+  const [currentCategory, setCurrentCategory] = useState<string>("");
 
-	const categories: string[] = [
-		...new Set(
-			articles.map((a) => a.pillarName).filter((pillarName) => pillarName),
-		),
-	];
+  const categories: string[] = [
+    ...new Set(
+      articles.map((a) => a.pillarName).filter((pillarName) => pillarName),
+    ),
+  ];
 
-	return (
-		<div className="container">
-			<SortBar
-				categories={categories}
-				setCurrentCategory={setCurrentCategory}
-			/>
-			<ArticlesContainer
-				articles={articles}
-				currentCategory={currentCategory}
-			/>
-		</div>
-	);
+  return (
+    <div className="container">
+      <SortBar
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+      />
+      <ArticlesContainer
+        articles={articles}
+        currentCategory={currentCategory}
+      />
+    </div>
+  );
 };
 
 export default HomePage;
