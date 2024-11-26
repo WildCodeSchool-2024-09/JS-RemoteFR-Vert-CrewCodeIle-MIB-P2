3034d4a06ba6ad55c53f24e33b2e768832640c74
MATHIEU FOUCAULT <foucault.mathieu.pro@gmail.com>
2024-11-26 11:51:02 +0100

    feat : favorites in sorbar #25

--- client/src/components/ArticlesContainer.tsx
+++ client/src/components/ArticlesContainer.tsx
@@ -9,11 +9,24 @@ export default function ArticlesContainer({
   articles: ArticleType[];
   currentCategory: string;
 }) {
+  const collectFavorites = () => {
+    const favorites = localStorage.getItem("favorites");
+    return favorites ? JSON.parse(favorites) : ([] as string[]);
+  };
+
+  const favoritesSelected = collectFavorites();
+
   return (
     <>
       <section className="articles-container">
         {articles
           .filter((a) => a.pillarName?.includes(currentCategory))
+          .map((a: ArticleType) => (
+            <Article key={a.id} {...a} />
+          ))}
+
+        {articles
+          .filter((a) => favoritesSelected?.includes(a.id))
           .map((a: ArticleType) => (
             <Article key={a.id} {...a} />
           ))}
